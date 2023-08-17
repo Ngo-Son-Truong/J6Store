dd6fa7161b4387593c65c3c5bfff672ae10d8482
Ngo-Son-Truong <ngosontruong75@gmail.com>
2023-08-17 21:40:12 +0700

    8.16

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -2,21 +2,34 @@ package com.example.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.service.OrderService;
 
 @Controller
 public class OrderController {
+	@Autowired
+	OrderService orderService;
+	
 	@RequestMapping("/order/checkout")
 	public String CheckOut() {
 		return "order/checkout";
 	}
 	@RequestMapping("/order/list")
-	public String List() {
+	public String List(Model model,HttpServletRequest request) {
+		String username = request.getRemoteUser();
+		model.addAttribute("orders",orderService.findByUsername(username));
 		return "order/list";
 	}
 	@RequestMapping("/order/detail/{id}")
-	public String Detail() {
+	public String detail(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("order",orderService.findById(id));
 		return "order/detail";
 	}
 }
--- src/main/resources/static/assets/js/shopping-cart-app.js
+++ src/main/resources/static/assets/js/shopping-cart-app.js
@@ -55,4 +55,32 @@ app.controller("shopping-cart-app",function ($scope,$http) {
 		}		
     }
     $scope.cart.loadFromLocalStorage();
+
+    $scope.order = {
+        createDate: new Date(),
+        address: "",
+        account: {username:$("#username").text()},
+        get orderDetails(){
+             return $scope.cart.items.map(item => {
+                return {
+                    product:{id: item.id},
+                    price: item.price,
+                    quantity: item.qty
+                }
+             });
+        },
+        purchase(){
+            var order = angular.copy(this);
+            //thực hiện đặt hàng
+            $http.post("/rest/orders",order).then(resp => {
+                alert("Đặt hàng thành công!");
+                $scope.cart.clear();
+                location.href = "/order/detail/" + resp.data.id;
+            }).catch(error => {
+                alert("Đặt hàng lỗi!")
+                console.log(error)
+            })
+        }
+    }
+
 })
